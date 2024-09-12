cd792da50428be67a40ec1d5684730a2dc1b1bf9
engle212 <engle.212@buckeyemail.osu.edu>
2024-09-12 10:25:36 -0400

    Made skeleton for interacting with Jetson

---
+++ slcapp/util.py
@@ -0,0 +1,25 @@
+# util.py: Utility functions
+# Maybe make a class here?
+
+# Establish a connection to Jetson Nano
+# Ethernet, Internet, or USB? Or run frontend on Nano?
+def connectToJetson():
+
+  return
+
+# Return list of all files on the Jetson
+# How does this get converted to be displayed in a navigatable format?
+def getFileSystem():
+
+  return
+
+# Get the contents of the given file
+def getFile(fileName):
+  
+  return
+
+# Create 3D representation of the given mesh
+# Feed this into HTML template as iframe?
+def displayMesh(fileName):
+
+  return
